b79b35b2c43dee71de7ba48f2b2af96c120a7e2d
Achhuta Nand Jha <achhutanandj@gmail.com>
2025-05-18 12:58:00 +0530

    Update app.py

--- app.py
+++ app.py
@@ -18,7 +18,7 @@ app.config['MAIL_SERVER'] = 'smtp.gmail.com'
 app.config['MAIL_PORT'] = 587
 app.config['MAIL_USE_TLS'] = True
 app.config['MAIL_USERNAME'] = 'achhutanandjha1@gmail.com'  # Replace with your email
-app.config['MAIL_PASSWORD'] = 'qonv llxu bvdy yoxi'  # Replace with your app password
+app.config['MAIL_PASSWORD'] = ''  # Replace with your app password
 
 mail = Mail(app)
 
